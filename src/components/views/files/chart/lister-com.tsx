0e736c58603c9baf827e929f7f8b1439d2a8b626
eyad sharaf almasri <iead.sh123@gmail.com>
2024-09-30 02:59:29 +0300

    Maximize and minimize table

--- src/components/views/files/chart/lister-com.tsx
+++ src/components/views/files/chart/lister-com.tsx
@@ -1,12 +1,16 @@
 import Card from '@/components/reusable/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { FC } from 'react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { BetweenHorizontalEnd, BetweenHorizontalStart } from 'lucide-react';
+import { FC, useState } from 'react';
 
 interface listerCom {
 	data: any;
 }
 
 const ListerCom: FC<listerCom> = ({ data }) => {
+	const [expansionOfColumns, setExpansionOfColumns] = useState(false);
+
 	const roundedNumber = (number: number, decimalPlaces: number) => {
 		return parseFloat(number.toFixed(decimalPlaces));
 	};
@@ -34,24 +38,64 @@ const ListerCom: FC<listerCom> = ({ data }) => {
 		{ header: 'Max Call' },
 	];
 
-	const headerFields = ['Agents', 'Service Level', 'Asa', 'Imm Answ', 'Pw', 'Occ', 'Max Call'];
+	const headers1 = [
+		{ header: 'From Time' },
+		{ header: 'To Time' },
+		{ header: 'Agents' },
+		{ header: 'Service Level' },
+		{ header: 'Service Level Target' },
+		{ header: 'Asa' },
+		{ header: 'Imm Answ' },
+		{ header: 'Pw' },
+		{ header: 'Occ' },
+		{ header: 'Max Call' },
+	];
 
+	const headerFields = ['Agents', 'Service Level', 'Asa', 'Imm Answ', 'Pw', 'Occ', 'Max Call'];
 	const bodyCellCassName = 'min-w-[150px] border-white/0 py-3 pr-4 text-[14px] text-navy-700 dark:text-white';
 
+	const handleExpansionOfColumns = () => setExpansionOfColumns(!expansionOfColumns);
 	return (
 		<Card extra={'w-full h-full sm:overflow-auto  mt-6 max-h-[450px]'}>
 			<Table className={' overflow-auto'}>
 				<TableHeader>
 					<TableRow key={'header'}>
-						{headers?.map((item) => {
+						{(expansionOfColumns ? headers : headers1)?.map((item) => {
 							return (
 								<TableHead
 									key={item.header}
 									className={`text-sm font-bold text-gray-600 dark:text-white pb-2 pr-4 pt-4 text-start ${
 										headerFields?.includes(item.header) ? 'bg-gray-100' : ''
-									}`}
+									} `}
 								>
-									{item.header}
+									<span className={item.header.toLowerCase() == 'to time' ? 'relative bottom-1' : ''}> {item.header}</span>
+									{item.header.toLowerCase() == 'to time' && (
+										<span className='relative left-[57%] cursor-pointer text-green-400'>
+											{!expansionOfColumns ? (
+												<TooltipProvider>
+													<Tooltip>
+														<TooltipTrigger>
+															<BetweenHorizontalStart onClick={handleExpansionOfColumns} />
+														</TooltipTrigger>
+														<TooltipContent>
+															<p>Preview all columns</p>
+														</TooltipContent>
+													</Tooltip>
+												</TooltipProvider>
+											) : (
+												<TooltipProvider>
+													<Tooltip>
+														<TooltipTrigger>
+															<BetweenHorizontalEnd onClick={handleExpansionOfColumns} />
+														</TooltipTrigger>
+														<TooltipContent>
+															<p>Preview fewer columns</p>
+														</TooltipContent>
+													</Tooltip>
+												</TooltipProvider>
+											)}
+										</span>
+									)}
 								</TableHead>
 							);
 						})}
@@ -66,17 +110,23 @@ const ListerCom: FC<listerCom> = ({ data }) => {
 							>
 								<TableCell className={bodyCellCassName}>{row.from_time}</TableCell>
 								<TableCell className={bodyCellCassName}>{row.to_time}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.totall_Offered}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.totall_answered}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.totall_abandoned}</TableCell>
-								<TableCell className={bodyCellCassName}>{roundedNumber(row.sl_xpercentage * 100, 2) + ' %'}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.sl_xseconds}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.callavg_talk_time}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.callavg_after_call_work}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.callavg_abandom}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.aggent_scheduled}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.aggent_logged_in}</TableCell>
-								<TableCell className={bodyCellCassName}>{row.aggent_available}</TableCell>
+								{expansionOfColumns ? (
+									<>
+										<TableCell className={bodyCellCassName}>{row.totall_Offered}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.totall_answered}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.totall_abandoned}</TableCell>
+										<TableCell className={bodyCellCassName}>{roundedNumber(row.sl_xpercentage * 100, 2) + ' %'}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.sl_xseconds}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.callavg_talk_time}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.callavg_after_call_work}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.callavg_abandom}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.aggent_scheduled}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.aggent_logged_in}</TableCell>
+										<TableCell className={bodyCellCassName}>{row.aggent_available}</TableCell>
+									</>
+								) : (
+									''
+								)}
 								<TableCell className={bodyCellCassName + ' bg-gray-100'}>{row.agents}</TableCell>
 								<TableCell
 									className={bodyCellCassName + ` bg-gray-100 ${roundedNumber(row.sl * 100, 2) < 80 ? 'bg-red-400' : 'bg-green-400'}`}
